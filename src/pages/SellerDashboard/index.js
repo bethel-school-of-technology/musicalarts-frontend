c36184a061a65a83703e4512b03157405bc58533
jextaposition <alexa.joy.hayes@gmail.com>
2021-09-29 16:38:36 -0700

    worked on the create listing component

--- src/pages/SellerDashboard/index.js
+++ src/pages/SellerDashboard/index.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import CreateListing from '../../components/CreateListing';
+// import Modal from '../../components/Modal/Modal';
+import './SellerDashboard.css';
 
 const SellerDashboard = () => {
-  return (
-    <>
-      {/* When the model's/backend are set up integrate it so that the user's name shows up */}
-      <h2>'first_name' Dashboard</h2>
-      <p>username: </p>
-      <p>first name:</p>
-      <p>last name: </p>
-      <p>email address:</p>
-      <p>account type: Seller</p>
+  const [modalButton, setModalButton] = useState(false);
 
-      <button>Create New Listing</button>
-      <button>Manage Listing's</button>
-      <button>Update Account</button>
-    </>
+  return (
+    <div>
+      <main>
+        {/* When the model's/backend are set up integrate it so that the user's name shows up */}
+        <h2>'first_name' Dashboard</h2>
+        <p>username: </p>
+        <p>first name:</p>
+        <p>last name: </p>
+        <p>email address:</p>
+        <p>account type: Seller</p>
+
+        <button onClick={() => setModalButton(true)}>Create New Listing</button>
+
+        <button>Manage Listing's</button>
+        <button>Update Account</button>
+        <CreateListing trigger={modalButton} setTrigger={setModalButton} />
+        {/* <Modal trigger={modalButton} setTrigger={setModalButton}>
+        <h2>this works right?</h2>
+      </Modal> */}
+      </main>
+    </div>
   );
 };
 

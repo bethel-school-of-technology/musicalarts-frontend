99d886b407627c73f69663df2c5d20c34092faa9
jextaposition <alexa.joy.hayes@gmail.com>
2021-10-21 23:50:47 -0700

    added bag total to nav

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -17,94 +17,85 @@ import navclass from './Navigation.module.css';
 function Navigation() {
   //const { countCartItems } = props;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const bag = JSON.parse(localStorage.getItem('shoppingBag')) || [];
+  const bagCount = bag.reduce((a, c) => a + c.bagQty, 0);
+
+  const user = JSON.parse(localStorage.getItem('user')) || [];
+  //const userExists = !!user;
+  // const token = localStorage.getItem('token');
+  // const isLoggedIn = !!token;
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     console.log(token);
+
     setIsLoggedIn(token);
   }, []);
 
+  //const bagCount = JSON.parse(localStorage.getItem('shoppingBag'));
+
   const history = useHistory();
   const signout = () => {
+    //localStorage.clear();
     localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setIsLoggedIn(false);
     history.push('/signin');
   };
-  // if(!token) {
-  //   return
-  // }
 
   return (
     <header className={navclass.header}>
-      {!isLoggedIn ? (
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/gallery'>Gallery</Link>
-            </li>
-            <li>
-              <Link to='/signin'>SignIn</Link>
-            </li>
-            <li>
-              <Link to='/signup'>SignUp</Link>
-            </li>
-            <li>
-              <Link to='/bag'>
-                ShopBag(0)
-                {/*{""}
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/gallery'>Gallery</Link>
+          </li>
+          <li>
+            <Link to='/bag'>
+              ShopBag({bagCount})
+              {/*{""}
               {countCartItems ? (
                 <button className="badge">{countCartItems}</button>
               ) : (
                 ""
               )}*/}
-                {/*<i className="fa fa-shopping-cart" aria-hidden="true"></i>*/}
-                {/*<FontAwesomeIcon icon={faShoppingCart} />*/}
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      ) : (
-        <nav>
-          <ul>
+              {/*<i classNarme="fa fa-shopping-cart" aria-hidden="true"></i>*/}
+              {/*<FontAwesomeIcon icon={faShoppingCart} />*/}
+            </Link>
+          </li>
+          {!isLoggedIn ? (
+            <>
+              {' '}
+              <li>
+                <Link to='/signin'>SignIn</Link>
+              </li>
+              <li>
+                <Link to='/signup'>SignUp</Link>
+              </li>
+            </>
+          ) : (
             <li>
-              <Link to='/'>Home</Link>
+              <UncontrolledDropdown>
+                <DropdownToggle caret>
+                  Welcome {user.user.username}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>
+                    <Link to='/dashboard'>Dashboard</Link>
+                  </DropdownItem>
+                  <DropdownItem onClick={signout}>Signout</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/gallery'>Gallery</Link>
-            </li>
-            <li>
-              <Link to='/bag'>
-                ShopBag(0)
-                {/*{""}
-              {countCartItems ? (
-                <button className="badge">{countCartItems}</button>
-              ) : (
-                ""
-              )}*/}
-                {/*<i className="fa fa-shopping-cart" aria-hidden="true"></i>*/}
-                {/*<FontAwesomeIcon icon={faShoppingCart} />*/}
-              </Link>
-            </li>
-            <UncontrolledDropdown>
-              <DropdownToggle caret>Welcome user</DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem header>
-                  <Link to='/dashboard'>Dashboard</Link>
-                </DropdownItem>
-                <DropdownItem onClick={signout}>Signout</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </ul>
-        </nav>
-      )}
+          )}
+        </ul>
+      </nav>
     </header>
   );
 }
